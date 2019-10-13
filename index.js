b2640ffcaefc6c83c575e9b077c00894a44b6fce
Abhirav Kumar <akabhirav@gmail.com>
2019-10-13 14:04:51 +0530

    fixed html send

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const app = require('express')();
 const port = process.env.PORT || 3000
+const fs = require('fs')
+const path = require('path')
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html')
+    res.sendFile('index.html', {root: __dirname })
 })
 
 app.listen(port, () => {
